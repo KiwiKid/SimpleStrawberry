02a99e48df9f1ff29d5c16d35680e6be9fc03407
KiwiKid <KiwiKid@users.noreply.github.com>
2021-06-05 10:34:31 +0000

    Deploying to gh-pages from  @ 432c31f9da0f04874b849192c02a129c6d26ebe6 🚀

--- textMap.js
+++ textMap.js
@@ -1,6 +1,25 @@
 const imageMap = [
     { text: 'Stunning 360° views', group: '1'},
     { text: 'All day sun with well sheltered areas', group: '1'},
-    { text: 'Over 3 acres of land, featuring a permaculture garden design', group: '1'}
+    { text: 'Over 3 acres of land, featuring a permaculture garden design', group: '1'},
+    { text: 'Living area heated by woodburner with integrated water heating', group: '1'},
+
+    { text: 'Second lounge with open fire', group: '2'},
+    { text: 'Insulated under floor and ceiling', group: '2'},
+    { text: '4 large bedrooms', group: '2'},
+    { text: 'Sunroom/study', group: '2'},
+
+    { text: 'Large laundry with walk-in linen room', group: '3'},
+    { text: 'Double garage with internal access', group: '3'},
+    { text: 'Bathroom with bath and separate shower', group: '3'},
+    { text: 'Situated 3 kilometres south west of Sawyers Bay in a rural setting, 10 kilometres north of the Octagon', group: '3'},
+    { text: 'The dwelling is very well orientated with the living areas having a northerly aspect', group: '3'},
+    { text: 'Handy to trendy Port Chalmers galleries and shops ', group: '3'},
+
+    { title: 'Permaculture' ,text: 'This property is based on permaculture principles and includes a integrated forest garden of fruit trees, berry bushes, and vegetables designed by Jason Ross. A herb spiral, raised veggie beds, glasshouse, and composting area. It also includes a worm farm, chicken coop, tool shed and a large barn. We are currently grazing 4 sheep, 4 chickens and a horse', group: '4'},
+    { text: 'This grazing 4 sheep, 4 chickens and a horse', group: '4'},
+    { title: 'History', text: 'Originally farmed by the Goddard family which grew vegetables and strawberries on this very fertile land.', group: '4'},
+    { text: 'If you would like more information, please reach out to me at <a src="mailto:southernwoman2@hotmail.com">southernwoman2@hotmail.com</a>', group: '4'}    
 ]
+
 export default imageMap
