26d186c68d81fe01326579e377242d531fac67b2
KiwiKid <KiwiKid@users.noreply.github.com>
2021-06-05 08:45:57 +0000

    Deploying to gh-pages from  @ ce4f95d3877133b671e9c3e01b155c648b19d3c0 🚀

---
+++ textMap.js
@@ -0,0 +1,6 @@
+const imageMap = [
+    { text: 'Stunning 360° views', group: '1'},
+    { text: 'All day sun with well sheltered areas', group: '1'},
+    { text: 'Over 3 acres of land, featuring a permaculture garden design', group: '1'}
+]
+export default imageMap
