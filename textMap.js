0c70683be36dfd79fccfee0f69fababc3048cd9d
KiwiKid <KiwiKid@users.noreply.github.com>
2021-06-05 23:51:38 +0000

    Deploying to gh-pages from  @ 9b8e4a9cb35776f22e60b679da574ae734447aa4 🚀

--- textMap.js
+++ textMap.js
@@ -1,4 +1,4 @@
-const imageMap = [
+const textMap = [
     { text: 'Stunning 360° views', group: '1'},
     { text: 'All day sun with well sheltered areas', group: '1'},
     { text: 'Over 3 acres of land, featuring a permaculture garden design', group: '1'},
@@ -16,10 +16,11 @@ const imageMap = [
     { text: 'The dwelling is very well orientated with the living areas having a northerly aspect', group: '3'},
     { text: 'Handy to trendy Port Chalmers galleries and shops ', group: '3'},
 
-    { title: 'Permaculture' ,text: 'This property is based on permaculture principles and includes a integrated forest garden of fruit trees, berry bushes, and vegetables designed by Jason Ross. A herb spiral, raised veggie beds, glasshouse, and composting area. It also includes a worm farm, chicken coop, tool shed and a large barn. We are currently grazing 4 sheep, 4 chickens and a horse', group: '4'},
-    { text: 'This grazing 4 sheep, 4 chickens and a horse', group: '4'},
-    { title: 'History', text: 'Originally farmed by the Goddard family which grew vegetables and strawberries on this very fertile land.', group: '4'},
-    { text: 'If you would like more information, please reach out to me at <a src="mailto:southernwoman2@hotmail.com">southernwoman2@hotmail.com</a>', group: '4'}    
+    { type: 'long', title: 'Permaculture' ,text: 'This property is based on permaculture principles and includes a integrated forest garden of fruit trees, berry bushes, and vegetables designed by Jason Ross. A herb spiral, raised veggie beds, glasshouse, and composting area. It also includes a worm farm, chicken coop, tool shed and a large barn. We are currently grazing 4 sheep, 4 chickens and a horse', group: '4'},
+    { type: 'long', title: 'History', text: 'Originally farmed by the Goddard family which grew vegetables and strawberries on this very fertile land.', group: '4'},
+
+    { text: 'If you would like more information, please reach out to me at <a src="mailto:southernwoman2@hotmail.com">southernwoman2@hotmail.com</a>', group: '5'}    
+
 ]
 
-export default imageMap
+export default textMap
